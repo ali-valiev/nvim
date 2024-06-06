505f47f73837919fe855d8cfa8cd9f93d48959b6
ali-valiev <alivaliev@proton.me>
2024-06-06 17:20:11 +0400

    modified lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,23 @@
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-		config = function()
-			require("lualine").setup({
-				options={
-					theme = 'auto',
-				}
-			})
-		end
+    config = function()
+        local function current_date()
+            return os.date('%A, %B %d, %Y %H:%M:%S')
+        end
+
+        require("lualine").setup({
+            options = {
+                theme = 'auto',
+            },
+            sections = {
+                lualine_a = {'mode'},
+                lualine_b = {},
+                lualine_c = {'filename'},
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {current_date}
+            },
+        })
+    end
 }
